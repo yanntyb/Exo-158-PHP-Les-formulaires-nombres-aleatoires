d7dbe070997d8d93c7a40f401c91ce83c15136be
Nux007 <007.nux@gmail.com>
2021-01-23 17:27:48 +0100

    Exo 158.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+- Créer un script php qui permet de générer un nombre aléatoire compris entre x et y
+- Coté front, créer un formulaire acceptant deux valeurs : x et y
+- Envoyer le formulaire au script php et retourner le nombre aleatoire généré
+
+
+- Pour générer un nombre aléatoire en PHP , vous pouvez utiliser la méthode mt_rand() ou rand()
+
+http://php.net/manual/fr/function.mt-rand.php
+*/
